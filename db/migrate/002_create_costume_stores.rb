7efde79bc5bcafc1532b6a18e8a5a459c887a27e
Scott Hoisington <hoyzington@gmail.com>
2020-01-27 03:31:49 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,9 +5,12 @@ class CreateCostumestores < ActiveRecord::Migration[5.2]
   def change
     create_table :costumestores do |c|
       t.string :name
-      t.float :location
-      t.string :size
-      t.string :image_url
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.string :opening_time
+      t.string :closing_time
       t.timestamps
     end
   end
