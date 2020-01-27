76a36f83636c627ab5d6e9942c97c1b6a7d5f520
Scott Hoisington <hoyzington@gmail.com>
2020-01-27 03:26:12 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,12 +1,14 @@
 # Create your costume_stores migration here
 
-class CreateCostumeStores < ActiveRecord::Migration
+class CreateCostumestores < ActiveRecord::Migration[5.2]
   
   def change
-    create_table :costume_stores do |c|
-      name
-      location
-      costume_inventory
+    create_table :costumestores do |c|
+      t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
     end
   end
   
