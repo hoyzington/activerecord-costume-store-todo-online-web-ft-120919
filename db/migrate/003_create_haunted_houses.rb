39a4d14eebf7e4df22e58b965c623d43da69f07a
Scott Hoisington <hoyzington@gmail.com>
2020-01-27 03:57:07 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,7 +3,7 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   
   def change
-    create_table :haunted_houses do |h|
+    create_table :haunted_house do |h|
       h.string :name
       h.string :location
       h.string :theme
