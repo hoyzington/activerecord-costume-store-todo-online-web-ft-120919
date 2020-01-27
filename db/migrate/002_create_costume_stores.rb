ad1cf54672f29599e81bc8a982a991fd11e69cbf
Scott Hoisington <hoyzington@gmail.com>
2020-01-27 03:17:33 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration
+  
+  def change
+    create_table :costume_stores do |c|
+      name
+      location
+      costume_inventory
+    end
+  end
+  
+end
